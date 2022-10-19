7daafa77bb53cfe632b77e380568c3bf5b892819
Schooly <marshallbcodes@gmail.com>
2022-10-18 22:13:55 -0500

    Update data-fetch.py

--- Husk/User Auth/data-fetch.py
+++ Husk/User Auth/data-fetch.py
@@ -1,4 +1,4 @@
-import sys, getpass, pwd
+import sys, getpass, pwd, spwd
 
 checkuser = getpass.getuser()
 username = sys.argv
@@ -12,5 +12,8 @@ print (('Home    :'), user_info.pw_dir)
 print (('Shell   :'), user_info.pw_shell)
 print("The user currently logged in is: " + checkuser)
 
+i = spwd.getspnam(checkuser)
 
-
+###FIGURE OUT HOW TO GET PERMISSION TO ACCESS SHADOW FILE###
+###ONCE WE HAVE ACCESS TO SHADOW FILE WE CAN THEN MAKE FEATURE###
+###ADD FEATURE TO CONFIRM USER PASSWORD BEFORE APP INSTALLATION###
