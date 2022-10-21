1a67b797695f992662650f97f95866a269021b59
Marshall B <110133622+SchoolyB@users.noreply.github.com>
2022-10-20 22:32:50 -0500

    Create window.py

---
+++ Husk/window.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+def openWindow():
+
+  window = Tk()
+
+  window.title = 'Husk version 0.3.1'
+  window.configure(width= 500, height= 500)
+  window.configure(bg='black')
+  window.mainloop()
