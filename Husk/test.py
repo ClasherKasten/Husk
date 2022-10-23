6f5803e478d77e73f5cdde70df41113f16629c9a
Schooly <marshallbcodes@gmail.com>
2022-10-23 15:31:09 -0500

    Add window gui w/ PyQt

--- Husk/test.py
+++
@@ -1,9 +0,0 @@
-###DO NOT DELETE UNTIL MORE WORK ON TKTERMINAL IS DONE###
-import tkinter as tk
-from tkterminal import Terminal
-import os
-
-root = tk.Tk()
-terminal = Terminal(pady=5, padx=5)
-terminal.pack(expand=True, fill='both')
-root.mainloop()
--- Husk/window.py
+++ Husk/window.py
@@ -1,33 +1,107 @@
-from tkinter import *
-import os
-from husk import main
-class Window(Frame):
-    def __init__(self, master=None):
-        Frame.__init__(self, master)
-        self.master = master
-
-        menu = Menu(self.master)
-        self.master.config(menu=menu)
-
-        fileMenu = Menu(menu)
-        fileMenu.add_command(label="Item")
-        fileMenu.add_command(label="Exit", command=self.exitProgram)
-        menu.add_cascade(label="File", menu=fileMenu)
-
-        editMenu = Menu(menu)
-        editMenu.add_command(label="Undo")
-        editMenu.add_command(label="Redo")
-        menu.add_cascade(label="Edit", menu=editMenu)
-
-    def exitProgram(self):
-        exit()
-        
-root = Tk()
-app = Window(root)
-root.wm_title("Husk Terminal")
+from PyQt5 import QtCore, QtGui, QtWidgets
 
 
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(920, 573)
+        MainWindow.setCursor(QtGui.QCursor(QtCore.Qt.PointingHandCursor))
+        MainWindow.setMouseTracking(False)
+        MainWindow.setDocumentMode(False)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.textEdit = QtWidgets.QTextEdit(self.centralwidget)
+        self.textEdit.setGeometry(QtCore.QRect(0, 0, 921, 531))
+        self.textEdit.setObjectName("textEdit")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 920, 22))
+        self.menubar.setObjectName("menubar")
+        self.menuFile = QtWidgets.QMenu(self.menubar)
+        self.menuFile.setObjectName("menuFile")
+        self.menuPreferences = QtWidgets.QMenu(self.menuFile)
+        self.menuPreferences.setObjectName("menuPreferences")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+        self.actionBackground_Color = QtWidgets.QAction(MainWindow)
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap("../assets/paintbrush.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.actionBackground_Color.setIcon(icon)
+        self.actionBackground_Color.setObjectName("actionBackground_Color")
+        self.actionCursor = QtWidgets.QAction(MainWindow)
+        icon1 = QtGui.QIcon()
+        icon1.addPixmap(QtGui.QPixmap("../assets/cursor.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.actionCursor.setIcon(icon1)
+        self.actionCursor.setObjectName("actionCursor")
+        self.actionFont = QtWidgets.QAction(MainWindow)
+        icon2 = QtGui.QIcon()
+        icon2.addPixmap(QtGui.QPixmap("../assets/font-size.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.actionFont.setIcon(icon2)
+        self.actionFont.setObjectName("actionFont")
+        self.actionExit = QtWidgets.QAction(MainWindow)
+        icon3 = QtGui.QIcon()
+        icon3.addPixmap(QtGui.QPixmap("../assets/cancel.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.actionExit.setIcon(icon3)
+        self.actionExit.setObjectName("actionExit")
+        self.actionNew = QtWidgets.QAction(MainWindow)
+        icon4 = QtGui.QIcon()
+        icon4.addPixmap(QtGui.QPixmap("../assets/new-page.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.actionNew.setIcon(icon4)
+        self.actionNew.setObjectName("actionNew")
+        self.actionOpen = QtWidgets.QAction(MainWindow)
+        self.actionOpen.setCheckable(False)
+        icon5 = QtGui.QIcon()
+        icon5.addPixmap(QtGui.QPixmap("../assets/document.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.actionOpen.setIcon(icon5)
+        self.actionOpen.setObjectName("actionOpen")
+        self.actionSave = QtWidgets.QAction(MainWindow)
+        icon6 = QtGui.QIcon()
+        icon6.addPixmap(QtGui.QPixmap("../assets/diskette.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        self.actionSave.setIcon(icon6)
+        self.actionSave.setObjectName("actionSave")
+        self.actionSave_As = QtWidgets.QAction(MainWindow)
+        self.actionSave_As.setObjectName("actionSave_As")
+        self.menuPreferences.addAction(self.actionBackground_Color)
+        self.menuPreferences.addAction(self.actionCursor)
+        self.menuPreferences.addAction(self.actionFont)
+        self.menuFile.addAction(self.actionNew)
+        self.menuFile.addAction(self.actionOpen)
+        self.menuFile.addSeparator()
+        self.menuFile.addAction(self.actionSave)
+        self.menuFile.addAction(self.actionSave_As)
+        self.menuFile.addSeparator()
+        self.menuFile.addAction(self.menuPreferences.menuAction())
+        self.menuFile.addSeparator()
+        self.menuFile.addAction(self.actionExit)
+        self.menuFile.addSeparator()
+        self.menubar.addAction(self.menuFile.menuAction())
 
-###THIS IS THE CALL TO OPEN WINDOW###
-root.mainloop()
-main()
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Husk Terminal"))
+        self.menuFile.setTitle(_translate("MainWindow", "File"))
+        self.menuPreferences.setTitle(_translate("MainWindow", "Preferences"))
+        self.actionBackground_Color.setText(_translate("MainWindow", "Background Color"))
+        self.actionCursor.setText(_translate("MainWindow", "Cursor"))
+        self.actionFont.setText(_translate("MainWindow", "Font"))
+        self.actionExit.setText(_translate("MainWindow", "Exit"))
+        self.actionExit.setShortcut(_translate("MainWindow", "Ctrl+Esc"))
+        self.actionNew.setText(_translate("MainWindow", "New..."))
+        self.actionOpen.setText(_translate("MainWindow", "Open..."))
+        self.actionSave.setText(_translate("MainWindow", "Save"))
+        self.actionSave_As.setText(_translate("MainWindow", "Save As"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
