3eaacda80fe917bdea81ef95c44d79b030a950da
Schooly <marshallbcodes@gmail.com>
2022-10-22 20:17:52 -0500

    Minor changes

--- Husk/User Auth/data-fetch.py
+++ Husk/User Auth/data-fetch.py
@@ -1,19 +1,53 @@
-import sys, getpass, pwd, spwd
+import getpass
+from time import sleep
 
-checkuser = getpass.getuser()
-username = sys.argv
-user_info = pwd.getpwnam(checkuser)
 
-print(('Username:'), user_info.pw_name)
-print (('Password:'), user_info.pw_passwd)
-print (('Comment :'), user_info.pw_gecos)
-print (('UID/GID :'), user_info.pw_uid, '/', user_info.pw_gid)
-print (('Home    :'), user_info.pw_dir)
-print (('Shell   :'), user_info.pw_shell)
-print("The user currently logged in is: " + checkuser)
+def Initial_Pass():
+  initialPass = input('Would you like to set up a password to use within the terminal?' '\n')
+  if initialPass == 'y':
+    Initial_Pass_Setup()
+  elif initialPass == 'n':
+    print('Ok. you can set up a password anytime by going to the preferences section and clicking "setup password" ')
+  else:
+    print('Please enter a valid response')
+    sleep(2)
+    Initial_Pass()
 
-i = spwd.getspnam(checkuser)
 
-###FIGURE OUT HOW TO GET PERMISSION TO ACCESS SHADOW FILE###
-###ONCE WE HAVE ACCESS TO SHADOW FILE WE CAN THEN MAKE FEATURE###
-###ADD FEATURE TO CONFIRM USER PASSWORD BEFORE APP INSTALLATION###
+
+def test():
+  
+  print('hello world')
+  
+  
+  
+def Initial_Pass_Setup():
+  userPass = getpass.getpass("please enter the password you would like to use")
+  confirmPass = getpass.getpass("please re-enter the password")
+  if userPass == confirmPass:
+    with open('user-data.txt','w', encoding='utf-8') as p:
+      p.write(userPass)
+
+
+def Reg_Pass_Prompt():
+  userPass = getpass.getpass("please enter the password you would like to use")
+  
+  
+  
+def Retrieve_Pass(userPass):
+  userData = open('user-data.txt', 'r')
+  userData.close()
+  
+  if userData == userPass:
+    ###RUN WHATEVER FUNCTION###
+    test()
+  elif userData != userPass:
+    print('Incorrect password please try again')
+  else:
+    print('incorrect password')###ADD MORE###
+      
+
+    
+###CALLING INITIAL PASS REQUEST###
+Initial_Pass()
+Retrieve_Pass(userPass)
